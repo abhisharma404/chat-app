3c4069cb1faee43edfb7bcd39f0679a4c6503a7e
Abhishek Sharma <abhishek_official@hotmail.com>
2018-06-09 11:41:26 +0530

    Geolocation API is added

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,15 @@ socket.on('disconnect',function(){
   console.log('Disconnected from server.');
 });
 
+socket.on('newLocationMessage',function(message){
+  li =jQuery('<li></li>');
+  a=jQuery('<a target="_blank">My current location</a>');
+  li.text(`${message.from}:`);
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+})
+
 jQuery('#message-form').on('submit',function(e){
   e.preventDefault();
   
@@ -24,4 +33,23 @@ jQuery('#message-form').on('submit',function(e){
     text: jQuery('[name=message]').val()
   })
 jQuery('[name=message]').val(" ");
+})
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click',function(e){
+console.log('Fetching location');
+  if (!navigator.geolocation){
+    return alert('Geo Location not supported by your browser.')
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position){
+    //console.log(position);
+    socket.emit('createLocationMessage',{
+      latitude : position.coords.latitude,
+      longitude : position.coords.longitude
+    })
+  },function(){
+    alert('Unable to fetch location!');
+  })
+
 })
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const port =process.env.PORT||3000;
 var app=express();
 var server=http.createServer(app);
 var io=socketIO(server);
-const {generateMessage}=require('./utils/message');
+const {generateMessage,generateLocationMessage}=require('./utils/message');
 
 app.use(express.static(publicPath));
 
@@ -27,7 +27,14 @@ socket.on('createMessage',(message)=>{
   io.emit('newMessage',generateMessage(message.from,message.text));
 })
 
+socket.on('createLocationMessage',(coords)=>{
+  io.emit('newLocationMessage',generateLocationMessage('User',coords.latitude,coords.longitude));
+  console.log('Recieved request');
+ });
+ 
+
 });
+
 
 server.listen(port,()=>{
   console.log(`Server is up on ${port}`);
